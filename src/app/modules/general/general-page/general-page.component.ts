dfff198cdf239871c350a7a33c28fd57fd0a94a0
Artur Vincho <vincho.artur@gmail.com>
2024-05-07 12:59:53 +0300

    add registration logic, change login logic, add token

--- src/app/modules/general/general-page/general-page.component.ts
+++ src/app/modules/general/general-page/general-page.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { VideoCallsComponent } from "@app/modules/calls/video.calls/video-calls.component";
 
 @Component({
   selector: 'progi-general-page',
   standalone: true,
-  imports: [],
   templateUrl: './general-page.component.html',
+  imports: [
+    VideoCallsComponent
+  ],
   styleUrl: './general-page.component.scss'
 })
 export class GeneralPageComponent {
